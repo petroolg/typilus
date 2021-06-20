6675c3d47c5d0fc32a2ae436918af3247cb7c753
Olga Petrova <olga.petrova@avast.com>
2021-06-21 01:05:20 +0200

    Prepare custom script for AST extraction.

---
+++ src/data_preparation/job.py
@@ -0,0 +1,18 @@
+"""
+Usage:
+    extractgraphs.py [options] SOURCE_FOLDER DUPLICATES_JSON SAVE_FOLDER TYPING_RULES
+
+Main file for data preprocessing. Walk recursively through the source folder's files to generate a code graph for each (concatenated in one jsonl output file) and a global type lattice.
+
+Options:
+    -h --help              Show this screen.
+    --debug                Debugging mode.
+"""
+
+from scripts.graph_generator import extract_graphs
+from docopt import docopt
+from dpu_utils.utils import save_jsonl_gz, run_and_debug
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    run_and_debug(lambda: extract_graphs.main(args), args['--debug'])
---
+++ src/data_preparation/scripts/prepare_data_custom.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd /usr/data
+
+###
+# Run deduplication. This assumes that .NET Core is installed.
+###
+
+git clone --depth=1 https://github.com/microsoft/near-duplicate-code-detector.git
+mkdir -p ./repo_tokens
+python3 ./near-duplicate-code-detector/tokenizers/python/tokenizepythoncorpus.py ./raw_repos/ ./repo_tokens/
+echo "In " $PWD
+dotnet run --project ./near-duplicate-code-detector/DuplicateCodeDetector/DuplicateCodeDetector.csproj -- --dir="./repo_tokens/" "./corpus_duplicates"
+
+###
+# Run AST extraction.
+###
+
+readonly SRC_BASE="/usr/src/datasetbuilder/scripts/"
+export PYTHONPATH="$SRC_BASE"
+mkdir -p graph-dataset
+python3 /usr/src/datasetbuilder/job.py  ./raw_repos/ ./corpus_duplicates.json ./graph-dataset $SRC_BASE/../metadata/typingRules.json
+mkdir -p graph-dataset-split
+python3 /usr/src/datasetbuilder/scripts/utils/split.py -data-dir ./graph-dataset -out-dir ./graph-dataset-split
